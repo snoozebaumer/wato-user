b686d7841f378f574ee767ef6e06142f5eeccffd
Samuel Nussbaumer <samuel.nussbaumer@stud.hslu.ch>
2024-02-13 17:12:58 +0100

    initialized services snoozebaumer/wato#1

---
+++ main.js
@@ -0,0 +1,25 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const server = express();
+
+server.use(bodyParser.json());
+
+server.post('/user', async (req, res) => {
+    console.log(req.body);
+    res.send("User posted");
+});
+
+server.get('/user/:id', async(req, res) => {
+    console.log(req.params.id);
+
+    const result = "User with id " + req.params.id + " found.";
+    if (result) {
+        res.send(result);
+    } else {
+        res.status(404);
+    }
+
+    res.end();
+});
+
+server.listen(4567);
